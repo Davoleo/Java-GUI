42d29f51e253df2e3b4fb8c4ff566a8402d72c73
Davoleo <davoleo2001@gmail.com>
2018-12-02 00:06:01 +0100

    First Commit | InputBox & MessageBox

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 01/12/2018 / 23:47
+ * Class: Main
+ * Project: JavaGUI
+ * Copyright - © - Davoleo - 2018
+ **************************************************/
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+
+        String fn = JOptionPane.showInputDialog("Enter first number");
+        String sn = JOptionPane.showInputDialog("Enter second number");
+
+        int num1 = Integer.parseInt(fn);
+        int num2 = Integer.parseInt(sn);
+        int sum = num1 + num2;
+
+        JOptionPane.showMessageDialog(null,"The sum of " + num1 + " and " + num2 + " is equals to " + sum,"Rouxls Kaard", JOptionPane.PLAIN_MESSAGE);
+
+    }
+}
