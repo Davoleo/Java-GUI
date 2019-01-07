dddb68a96a631945b0699f6e67849920489ab89e
Davoleo <davoleo2001@gmail.com>
2019-01-07 20:42:30 +0100

    IntelliJ Gui Designer Test

---
+++ src/swing/practice/GuiTest.java
@@ -0,0 +1,22 @@
+package swing.practice;
+
+import javax.swing.*;
+
+/*************************************************
+ * Author: Davoleo
+ * Date / Hour: 07/01/2019 / 20:31
+ * Class: GuiTest
+ * Project: JavaGUI
+ * Copyright - © - Davoleo - 2018
+ **************************************************/
+
+public class GuiTest {
+    private JButton button1;
+    private JSlider slider1;
+    private JComboBox comboBox1;
+    private JTree tree1;
+    private JSpinner spinner1;
+    private JCheckBox checkBox1;
+    private JCheckBox checkBox2;
+    private JRadioButton radioButton1;
+}
