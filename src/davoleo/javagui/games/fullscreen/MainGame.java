7325371af3112eddad85b00e85c964374252aa4b
Davoleo <davoleo2001@gmail.com>
2019-07-14 15:57:24 +0200

    Adjust Main to the new ScreenManager

--- src/davoleo/javagui/games/fullscreen/MainGame.java
+++ src/davoleo/javagui/games/fullscreen/MainGame.java
@@ -15,29 +15,38 @@ import java.awt.*;
 
 public class MainGame extends JFrame {
 
-    public static void main(String[] args)
-    {
-        DisplayMode dm = new DisplayMode(800, 600, 16, DisplayMode.REFRESH_RATE_UNKNOWN);
-        MainGame main = new MainGame();
-        main.run(dm);
-    }
-
     private ScreenManager screen;
     private Image bg;
+    private Image js;
     private Image pic;
     private Animation animation;
     private boolean loaded;
 
-    private void run(DisplayMode dm)
+    private static final DisplayMode[] modes = {
+            new DisplayMode(1920, 1080, DisplayMode.BIT_DEPTH_MULTI, DisplayMode.REFRESH_RATE_UNKNOWN),
+            new DisplayMode(800, 600, 32, DisplayMode.REFRESH_RATE_UNKNOWN),
+            new DisplayMode(800, 600, 24, DisplayMode.REFRESH_RATE_UNKNOWN),
+            new DisplayMode(800, 600, 16, DisplayMode.REFRESH_RATE_UNKNOWN),
+            new DisplayMode(640, 480, 32, DisplayMode.REFRESH_RATE_UNKNOWN),
+            new DisplayMode(604, 480, 24, DisplayMode.REFRESH_RATE_UNKNOWN),
+            new DisplayMode(604, 480, 16, DisplayMode.REFRESH_RATE_UNKNOWN)
+    };
+
+    public static void main(String[] args)
     {
-        setBackground(Color.PINK);
-        setForeground(Color.BLACK);
+        MainGame main = new MainGame();
+        main.run();
+    }
+
+    private void run()
+    {
         setFont(new Font("Arial", Font.PLAIN, 24));
         loaded = false;
 
         screen = new ScreenManager();
         try {
-            screen.setFullScreen(dm, this);
+            DisplayMode dm = screen.findFirstCompatibleMode(modes);
+            screen.setFullScreen(dm);
             loadImages();
             movieLoop();
         }finally {
@@ -47,11 +56,13 @@ public class MainGame extends JFrame {
 
     private void loadImages()
     {
-        bg = new ImageIcon(getClass().getResource("../../../../resources/js.png")).getImage();
+        bg = new ImageIcon(getClass().getResource("../../../../resources/forest_bg.jpg")).getImage();
+        js = new ImageIcon(getClass().getResource("../../../../resources/js.png")).getImage();
         pic = new ImageIcon(getClass().getResource("../../../../resources/40x40.png")).getImage();
 
         Image face1 = new ImageIcon(getClass().getResource("../../../../resources/happy.png")).getImage();
         Image face2 = new ImageIcon(getClass().getResource("../../../../resources/sad.png")).getImage();
+
         animation = new Animation();
         animation.addScene(face1, 250);
         animation.addScene(face2, 250);
@@ -71,9 +82,10 @@ public class MainGame extends JFrame {
             animTime += timePassed;
             animation.update(timePassed);
 
-            Graphics graphics = screen.getFullScreenWindow().getGraphics();
-            graphics.drawImage(animation.getFrame(), 0, 0, null);
+            Graphics2D graphics = screen.getGraphics();
+            paint(graphics);
             graphics.dispose();
+            screen.update();
 
             try {
                 Thread.sleep(20);
@@ -90,8 +102,10 @@ public class MainGame extends JFrame {
             ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
 
         if (loaded) {
-            g.drawImage(bg, 200, 200, null);
-            g.drawImage(pic, 1000, 500, null);
+            g.drawImage(bg, 0, 0, null);
+            g.drawImage(js, 0, 0, null);
+            g.drawImage(pic, 50, 50, null);
+            g.drawImage(animation.getFrame(), 300, 50, null);
         }
         g.drawString("Awesome!", 200, 200);
     }
